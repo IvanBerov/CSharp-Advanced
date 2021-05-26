98e9b5941854e07e91c868377a3362f5e5c5f99a
Ivan Berov <ivanberov3@gmail.com>
2021-05-26 21:24:21 +0200

    exam prep

---
+++ ExamPreparation/Advanced Exam - 28 June 2020/Bombs/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bombs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] bombEffects = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] bombCasing = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Stack<int> stack = new Stack<int>(bombCasing);
+            Queue<int> queue = new Queue<int>(bombEffects);
+
+            int datura = 0;
+            int cherry = 0;
+            int smokeDecoy = 0;
+
+            while (true)
+            {
+                int currBombEffect = queue.Peek();
+                int currBombCasing = stack.Peek();
+                int sum = currBombCasing + currBombEffect;
+
+                if (sum == 40)
+                {
+                    datura++;
+                    queue.Dequeue();
+                    stack.Pop();
+                }
+                else if (sum == 60)
+                {
+                    cherry++;
+                    queue.Dequeue();
+                    stack.Pop();
+                }
+                else if (sum == 120)
+                {
+                    smokeDecoy++;
+                    queue.Dequeue();
+                    stack.Pop();
+                }
+                else
+                {
+                    sum -= 5;
+
+                    while (true)
+                    {
+                        if (sum == 40)
+                        {
+                            datura++;
+                            queue.Dequeue();
+                            stack.Pop();
+                            break;
+                        }
+                        else if (sum == 60)
+                        {
+                            cherry++;
+                            queue.Dequeue();
+                            stack.Pop();
+                            break;
+                        }
+                        else if (sum == 120)
+                        {
+                            smokeDecoy++;
+                            queue.Dequeue();
+                            stack.Pop();
+                            break;
+                        }
+                        else
+                        {
+                            sum -= 5;
+                        }
+
+                    }
+
+                }
+                if (queue.Count == 0 ||
+                    stack.Count == 0 ||
+                    (datura >= 3 && cherry >= 3 && smokeDecoy >= 3))
+                {
+                    break;
+                }
+            }
+            if (datura >= 3 && cherry >= 3 && smokeDecoy >= 3)
+            {
+                Console.WriteLine("Bene! You have successfully filled the bomb pouch!");
+            }
+            else
+            {
+                Console.WriteLine("You don't have enough materials to fill the bomb pouch.");
+            }
+            if (queue.Count == 0)
+            {
+                Console.WriteLine("Bomb Effects: empty");
+            }
+            else
+            {
+                Console.WriteLine($"Bomb Effects: {string.Join(", ", queue)} ");
+            }
+            if (stack.Count == 0)
+            {
+                Console.WriteLine("Bomb Casings: empty");
+            }
+            else
+            {
+                Console.WriteLine($"Bomb Casings: {string.Join(", ", stack)}");
+            }
+            Console.WriteLine($"Cherry Bombs: {cherry}");
+            Console.WriteLine($"Datura Bombs: {datura}");
+            Console.WriteLine($"Smoke Decoy Bombs: {smokeDecoy}");
+        }
+    }
+}
---
+++ ExamPreparation/Advanced Exam - 28 June 2020/Snake/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            char[,] matrix = new char[n, n];
+
+            int snakeRow = -1;
+            int snakeCol = -1;
+            int firsBurrowRow = -1;
+            int firsBurrowCol = -1;
+            int secondBurrowRow = -1;
+            int secondBurrowCol = -1;
+
+            for (int row = 0; row < n; row++)
+            {
+                string line = Console.ReadLine();
+
+                for (int col = 0; col < line.Length; col++)
+                {
+                    matrix[row, col] = line[col];
+
+                    if (matrix[row, col] == 'S')
+                    {
+                        snakeRow = row;
+                        snakeCol = col;
+                    }
+                    if (matrix[row,col] == 'B')
+                    {
+                        if (firsBurrowRow == -1)
+                        {
+                            firsBurrowRow = row;
+                            firsBurrowCol = col;
+                        }
+                        else
+                        {
+                            secondBurrowRow = row;
+                            secondBurrowCol = col;
+                        }
+                    }
+                }
+            }
+
+            matrix[snakeRow, snakeCol] = '.';
+
+            int foodCount = 0;
+
+            while (true)
+            {
+                if (foodCount >= 10)
+                {
+                    Console.WriteLine("You won! You fed the snake.");
+                    matrix[snakeRow, snakeCol] = 'S';
+                    break;
+                }
+
+                string command = Console.ReadLine();
+
+                if (command == "up")
+                {
+                    snakeRow--;
+                }
+                if (command == "down")
+                {
+                    snakeRow++;
+                }
+                if (command == "left")
+                {
+                    snakeCol--;
+                }
+                if (command == "right")
+                {
+                    snakeCol++;
+                }
+
+                if (!IsSafePosition(matrix, snakeRow, snakeCol))
+                {
+                    Console.WriteLine("Game over!");
+                    break;
+                }
+                if (matrix[snakeRow, snakeCol] == '*')
+                {
+                    foodCount++;
+                }
+                if (matrix[snakeRow,snakeCol] == 'B')
+                {
+                    matrix[snakeRow, snakeCol] = '.';
+
+                    if (firsBurrowRow == snakeRow 
+                     && firsBurrowCol == snakeCol) // if we are on the firs Burrow
+                    {                              // we set it to second Burrow      
+                        snakeRow = secondBurrowRow;
+                        snakeCol = secondBurrowCol;
+                    }
+                    else
+                    {
+                        snakeRow = firsBurrowRow;
+                        snakeCol = firsBurrowCol;
+                    }
+                }
+                matrix[snakeRow, snakeCol] = '.';
+                //Print(matrix);
+            }
+
+            Console.WriteLine($"Food eaten: {foodCount}");
+            Print(matrix);
+
+        }
+        static void Print(char[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+                Console.WriteLine();
+            }
+        }
+        static bool IsSafePosition(char[,] matrix, int row, int col)
+        {
+            if (row < 0 || col < 0)
+            {
+                return false;
+            }
+            if (row >= matrix.GetLength(0) ||
+                col >= matrix.GetLength(1))
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
+
